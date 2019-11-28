a5374c168c7a3bee3f197082480b2f7a0b5ca9f2
Julio Adán Pérez García <julioperezag@gmail.com>
2019-11-27 23:14:01 -0600

    Fix var names

--- src/com/rfc/modelos/Datos2.java
+++ src/com/rfc/modelos/Datos2.java
@@ -1,12 +1,19 @@
+package com.rfc.modelos;
 
-public class Datos2 extends Datos{
-	protected String nomCom;
-	
-	public Datos2(String nomCom,String nomb, String apeP,String  apeM,int dia,int mes, int año,String x,String y,String rfc) {
-		super( nomb, apeP, apeM,dia, mes, año,x,y,rfc);
-		this.nomCom=nomCom;
-	}
-	public void setNomCom(String nomCom) {this.nomCom=nomCom;}
-	public String getNomCom() {return this.nomCom;}
-	
+public class Datos2 extends Datos {
+    protected String nomCom;
+
+    public Datos2(String nomCom, String nomb, String apeP, String apeM, int dia, int mes, int anio, String x, String y, String rfc) {
+        super(nomb, apeP, apeM, dia, mes, anio, x, y, rfc);
+        this.nomCom = nomCom;
+    }
+
+    public void setNomCom(String nomCom) {
+        this.nomCom = nomCom;
+    }
+
+    public String getNomCom() {
+        return this.nomCom;
+    }
+
 }
